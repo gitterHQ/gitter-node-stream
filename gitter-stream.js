6cb995973b9e84a4dad941f6b9107421f8262f5e
melvincarvalho <melvincarvalho@gmail.com>
2015-02-05 19:44:58 +0100

    change http to https

--- gitter-stream.js
+++ gitter-stream.js
@@ -1,4 +1,4 @@
-var http = require('http');
+var https = require('https');
 
 var roomId    = process.env.ROOM_ID;
 var token     = process.env.TOKEN;
@@ -6,13 +6,13 @@ var heartbeat = " \n";
 
 var options = {
   hostname: 'stream.gitter.im',
-  port:     80,
+  port:     443,
   path:     '/v1/rooms/' + roomId + '/chatMessages',
   method:   'GET',
   headers:  {'Authorization': 'Bearer ' + token}
 };
 
-var req = http.request(options, function(res) {
+var req = https.request(options, function(res) {
   res.on('data', function(chunk) {
     var msg = chunk.toString();
     if (msg !== heartbeat) console.log('Message: ' + msg);
